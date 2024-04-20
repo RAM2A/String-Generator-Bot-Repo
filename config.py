0d04b8560ad8ee14377060b175d3dfdfb1cd339e
RAM2A <160558024+RAM2A@users.noreply.github.com>
2024-04-20 11:04:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 API_ID = "24665357"
 API_HASH = "beb7e4b83ada668fa85f9a9b56338f1d"
 
-BOT_TOKEN = "7040213172:AAELc-DaZ7_ROVqfVejkE_t0jy00trExCc4"
+BOT_TOKEN = "6743842034:AAGXsmltL96GQm0-eDo2J5XX2X9kvD-VF9s"
 OWNER_ID = "1717511106"
 
 MONGO_DB_URI = "mongodb+srv://subham94mii2:7wRYh7ylrGEsFGkP@busybot.oskm6vj.mongodb.net/?retryWrites=true&w=majority&appName=busybot"
-MUST_JOIN = "dev_gagan"
+MUST_JOIN = "viewxy"
